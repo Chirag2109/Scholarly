4c07abde2d107fd16256453f6ed7b8ae24b51d1d
Chirag2109 <mittalchirag010@gmail.com>
2024-11-29 16:28:06 +0530

    added articles in the article page

---
+++ BackEnd/app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import userRoute from "./routes/user.js";
+import "./config/dbconnection.js";
+
+const app = express();
+const port = "3000";
+
+// Generic Middlewares
+app.use(express.json());
+app.use("*", (req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, PUT, POST, DELETE, OPTIONS"
+  );
+  next();
+});
+
+app.use("/", express.static("./../frontend/dist"))
+
+// Routing middleware
+app.use("/user", userRoute);
+app.listen(port, () => {
+  console.log("The server is running on port: ", port);
+});
